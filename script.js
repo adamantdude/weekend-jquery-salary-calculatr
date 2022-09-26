f542490bc9add5e44d4c3a151d1496354bc3f84e
adamantdude <adam.plee23@gmail.com>
2022-09-26 11:54:45 -0500

    code revision with in-class notes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 $(main);
 
+let employees = [];
+
 let totalMonthly = 0;
 
 function main() {
@@ -16,21 +18,21 @@ function onInitialize() {
 }
 
 function submitEmployee() {
-    $('#emBody').append(`
-        <tr>
-            <td>${$('#fName').val()}</td>
-            <td>${$('#lName').val()}</td>
-            <td>${$('#emID').val()}</td>
-            <td>${$('#emTitle').val()}</td>
-            <td>$${$('#emSalary').val()}</td>
-            <td><button class="deleteEm">Delete</button></td>
-        </tr>
-    `)
+
+    employee = {
+        fName: $('#fName').val(),
+        lName: $('#lName').val(),
+        emID: $('#emID').val(),
+        emTitle: $('#emTitle').val(),
+        emSalary: $('#emSalary').val()
+    };
+
+    employees.push(employee);
 
     // calculate new total monthly
     totalMonthly += ($('#emSalary').val() / 12);
     // then change it visually on the DOM
-    changeMonthly();
+    render();
 
     // reset text box values
     $('#fName').val('');
@@ -43,11 +45,13 @@ function submitEmployee() {
 
 // main function for removing table rows and calculating Total Monthly deductions
 function removeEmployee() {
+    index = $(this).parent().parent().attr('id');
+    employees.splice(index, 1);
     //button > td > tr > remove();
     $(this).parent().parent().remove();
-    //              button >  td   >   tr   >  td(6)  >   td[4]  > string > $**** > ****
-    totalMonthly -= ($(this).parent().parent().children().slice(4,5).text().slice(1)/12);
-    changeMonthly();
+    //              button >  td   >   tr   >   td[4]  > string > $**** > ****
+    totalMonthly -= ($(this).parent().siblings('#emSal').text().slice(1) / 12);
+    render();
 }
 
 // main function to change the Total Monthly element
@@ -61,4 +65,22 @@ function changeMonthly() {
     
     //if it toggles, it can mirror itself
     totalMonthly > 20000 ? $('#totalSection h2').toggleClass('overLimit') : null ;
+}
+
+function render() {
+    $('#emBody').empty();
+
+    for(let i=0; i<employees.length; ++i) {
+        $('#emBody').append(`
+            <tr id=${i}>
+                <td>${employees[i].fName}</td>
+                <td>${employees[i].lName}</td>
+                <td>${employees[i].emID}</td>
+                <td>${employees[i].emTitle}</td>
+                <td id="emSal">$${employees[i].emSalary}</td>
+                <td><button class="deleteEm">Delete</button></td>
+            </tr>
+        `)
+    }
+    changeMonthly();
 }
